9ef05dcfc4b098230efdcb171d3cadee0b7f3980
Anthony Shewnarain <anthony.shewnarain@gmail.com>
2018-06-04 20:48:43 -0400

    created logs directory

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd /tmp
+mkdir logs
 ls -l
-nohup java -jar target/smartbox-1.0-SNAPSHOT.jar server config.yml > /var/log/smartbox.out 2>&1
+nohup java -jar target/smartbox-1.0-SNAPSHOT.jar server config.yml > logs/smartbox.out 2>&1
