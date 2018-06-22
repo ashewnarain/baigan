7dbf05ca26712d2bd05f4428f4bcc17fb9839a93
Anthony Shewnarain <anthony.shewnarain@gmail.com>
2018-06-21 22:05:50 -0400

    Updated scripts

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /tmp
 ls -l
-nohup java -jar target/smartbox-1.0-SNAPSHOT.jar server config.yml > logs/smartbox.out 2>&1 &
+nohup java -jar target/smartbox-1.0-SNAPSHOT.jar server config.yml > /home/ec2-user/smartbox.out 2>&1 &
